07ee0c094bd712aa860e327ca055c24f113f1f5a
Alcr33k <alcr33k@gmail.com>
2019-02-21 19:07:39 +0100

    Fixed solution

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 // Node module that prints N dates between X months
-var nMonths = 6;
-var numDates = 40;
-var startDate = new Date();
-
-console.log(getCurrentdate(startDate));
+var nMonths = 6; // difference in months between the dates
+var numDates = 40; // how many dates to print
+var startDate = new Date(); // current date
+var endDate = new Date();
+endDate.setMonth(endDate.getMonth()+nMonths); 
 
 for (var i = 0; i < numDates; i++) {
-    var mToAdd = getRandomInt(0, nMonths);
-    var dToAdd = getRandomInt(0, 20)
+    var res = new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
+    console.log(formatDate(res));
 }
 
-function getCurrentdate(inputDate) {
+function formatDate(inputDate) {
     var dd = inputDate.getDate();
     var mm = inputDate.getMonth() + 1; //January is 0!
     var yyyy = inputDate.getFullYear();
     if (dd < 10) {
-    dd = '0' + dd;
+        dd = '0' + dd;
     } 
     if (mm < 10) {
-    mm = '0' + mm;
+        mm = '0' + mm;
     } 
     var res = yyyy + '-' + mm + '-' + dd;
     return res;
-}
-
-function getRandomInt(min, max) {
-    return Math.random() * (max - min) + min;
 }
